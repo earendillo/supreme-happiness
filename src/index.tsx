e0ba82768c728fc34d315b8f462bdbb6bf3af381
Mateusz Matusiak <mateusz.matusiak@unitygroup.com>
2022-03-09 21:31:41 +0100

    starterpack ready

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Home } from './components/Home.component'
+
+ReactDOM.render(
+    <Home />,
+    document.getElementById('app')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ['ts-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
+    },
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: './public',
+        port: 3000
+    },
+    devtool: "source-map"
+};
